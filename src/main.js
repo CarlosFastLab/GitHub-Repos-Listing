539b8b4668774edeae5c63fc62883eb4a6969588
CarlosFastLab <fastcaf@hotmail.com>
2021-08-16 20:21:25 -0300

    early features implementation

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-document.querySelector('body').style.background = 'red';
+class App {
+    // Construtor
+    constructor() {
+        // Lista de repositórios
+        this.repostorios = [];
+
+        // Form
+        this.formulario = document.querySelector('form');
+
+        // Método para registrar eventos do form
+        this.registrarEventos();
+    }
+
+    registrarEventos() {
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
+    }
+
+    adicionarRepositorio(evento) {
+        // Evita que o formulário recarregue a página
+        evento.preventDefault();
+
+        // Adiciona o repositório na lista
+        this.repostorios.push({
+            nome: 'Nerd Fonts',
+            descricao: 'Iconic font aggregator, collection, and patcher',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/8083459?v=4',
+            link: 'https://github.com/ryanoasis/nerd-fonts'
+        });
+
+        // Renderizar a tela
+        console.log(this.repostorios);
+    }
+}
+
+new App();
